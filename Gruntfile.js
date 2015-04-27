7acc990247c80d9b8bcf41841fd43b290e50572b
John D. Storey <johndangerstorey@gmail.com>
2015-04-26 19:16:12 -0600

    initializes grunt with package.json to watch less directory

---
+++ Gruntfile.js
@@ -0,0 +1,29 @@
+module.exports = function(grunt) {
+  require('jit-grunt')(grunt);
+
+  grunt.initConfig({
+    less: {
+      development: {
+        options: {
+          compress: true,
+          yuicompress: true,
+          optimization: 2
+        },
+        files: {
+          "assets/style/css/main.css": "assets/style/less/main.less" // destination file and source file
+        }
+      }
+    },
+    watch: {
+      styles: {
+        files: ['assets/style/less/**/*.less'], // which files to watch
+        tasks: ['less'],
+        options: {
+          nospawn: true
+        }
+      }
+    }
+  });
+
+  grunt.registerTask('default', ['less', 'watch']);
+};
